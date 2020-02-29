bb11b75a274f62d99bf2e26f235bbe2c454a6e53
thinker <731666148@qq.com>
2020-02-29 14:41:44 +0800

    修改导航栏属性

--- SwiftUI-Buildin/ElementListView.swift
+++ SwiftUI-Buildin/ElementListView.swift
@@ -8,18 +8,30 @@
 
 import SwiftUI
 
-
-
 struct ElementListView: View {
         
+    //修改导航栏文字颜色
+     init() {
+           UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.systemRed]
+           UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.systemRed]
+           UINavigationBar.appearance().tintColor = .systemRed
+       }
+    
     var body: some View {
+        
         NavigationView(){
             List{
                 NavigationLink(destination:SysTableView()){
-                    Text("TableView")
+                    VStack(alignment: .leading){
+                        Text("TableView")
+                    }
                 }
-                
-            }.navigationBarTitle("SwiftUI自带组件用法")
+                NavigationLink(destination:SysNavigationView()){
+                    VStack(alignment: .leading){
+                        Text("NavigationView")
+                    }
+                }
+            }.navigationBarTitle("SwiftUI自带组件用法", displayMode: .inline)
         }
     }
 }
---
+++ SwiftUI-Buildin/VIews/SysNavigationView.swift
@@ -0,0 +1,21 @@
+//
+//  SysNavigationView.swift
+//  SwiftUI-Buildin
+//
+//  Created by cfans on 2020/2/28.
+//  Copyright © 2020 cfans. All rights reserved.
+//
+
+import SwiftUI
+
+struct SysNavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).navigationBarTitle("NavigationView", displayMode: .large)
+    }
+}
+
+struct SysNavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        SysNavigationView()
+    }
+}
